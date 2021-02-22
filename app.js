baf9f90f6f646fd91a60b05f6143810e2b089690
Mawyn NHEK <79303836+Nezertiam@users.noreply.github.com>
2021-02-22 17:17:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $('#burger').click(function(){
-        $('.navbar-alt').toggleClass('isOpen');
-    })
-});
+document.getElementById("burger").addEventListener("click", () => {
+    document.getElementById("navigation").classList.toggle("navPop");
+    document.querySelector("main").classList.toggle("navIsOpen");
+    document.getElementById("burger").classList.toggle("fa-bars");
+    document.getElementById("burger").classList.toggle("fa-times");
+})
